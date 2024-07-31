3bb2e10f6628a4389974759cdc37080e9f7ed0f1
oakinh <nikaohill@gmail.com>
2024-07-31 09:39:24 -0600

    Add serving from root, and logo.png

--- main.go
+++ main.go
@@ -7,6 +7,7 @@ import (
 func main() {
 
 	serveMux := http.NewServeMux()
+	serveMux.Handle("/", http.FileServer(http.Dir(".")))
 	server := http.Server{Addr: "localhost:8080", Handler: serveMux}
 	server.ListenAndServe()
 
